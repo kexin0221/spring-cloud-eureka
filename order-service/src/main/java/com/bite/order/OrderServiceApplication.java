14cca75b1fcd40a615e841e99216a4ae015fbe4a
mou-kexin <2584857586@qq.com>
2026-08-29 10:20:35 +0800

    cloud学习代码

--- order-service/src/main/java/com/bite/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/bite/order/OrderServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
-public class orderServiceApplication {
+public class OrderServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(orderServiceApplication.class, args);
+        SpringApplication.run(OrderServiceApplication.class, args);
     }
 }
